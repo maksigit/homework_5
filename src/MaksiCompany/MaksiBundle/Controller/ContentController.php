f9076515c904c9fe3ca3a47e020d4557e9fd01be
Maksim Kholod <maksi123p@gmail.com>
2017-11-28 13:41:32 +0200

    doctrine

--- src/MaksiCompany/MaksiBundle/Controller/ContentController.php
+++ src/MaksiCompany/MaksiBundle/Controller/ContentController.php
@@ -12,13 +12,22 @@ class ContentController extends Controller
 {
 
     /**
-     * @Route("/content", name="content")
+     * @Route("/content/", name="content")
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function ContentAction(Request $request)
     {
 
-        return $this->render('MaksiCompanyMaksiBundle:content:content.html.twig');
+//        $this->get('Paran')->perim(25, 98);
+//        $posts = [
+//            'post1',
+//            'post2',
+//            'post3',
+//            'post4',
+//        ];
+        return $this->render('MaksiCompanyMaksiBundle:content:content.html.twig', [
+//            'posts' => $posts
+        ]);
 
     }
 }
--- src/MaksiCompany/MaksiBundle/Controller/DefaultController.php
+++ src/MaksiCompany/MaksiBundle/Controller/DefaultController.php
@@ -7,9 +7,38 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use MaksiCompany\MaksiBundle\Entity\Product;
 
 class DefaultController extends Controller
 {
+
+    public function createAction()
+    {
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to your action: createAction(EntityManagerInterface $em)
+        $em = $this->getDoctrine()->getManager();
+
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(19.99);
+        $product->setDescription('Ergonomic and stylish!');
+
+        // tells Doctrine you want to (eventually) save the Product (no queries yet)
+        $em->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $em->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+
+// if you have multiple entity managers, use the registry to fetch them
+    public function editAction()
+    {
+        $doctrine = $this->getDoctrine();
+        $em = $doctrine->getManager();
+        $em2 = $doctrine->getManager('other_connection');
+    }
 
     /**
      * @Route("/", name="home")
@@ -18,9 +47,9 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         //$this->generateUrl('blog', array('page' => 2)) ;
-        var_dump($request->server->all());
-        var_dump($_SERVER);
-        exit;
+//        var_dump($request->server->all());
+//        var_dump($_SERVER);
+//        exit;
         return $this->render('MaksiCompanyMaksiBundle:Default:index.html.twig');
         //return new Response('foo');
     }
@@ -34,4 +63,7 @@ class DefaultController extends Controller
         //return $this->render('MaksiCompanyMaksiBundle:Default:blog.html.twig');
         return new JsonResponse(array('page' => $page));
     }
+
+
+
 }
